2b2053e8e44e7dba0e89f5a2fb25d75a35368626
TomoyaOnishi <tomoyaonishi@icloud.com>
2018-10-01 13:47:13 +0900

    fix

--- src/containers/TodoContainer.ts
+++ src/containers/TodoContainer.ts
@@ -1,21 +1,23 @@
 import {connect} from 'react-redux';
+import {Dispatch} from 'redux';
 import {todoActions} from '../actions/TodoActions';
 import {Action} from 'typescript-fsa';
 import {IAppState} from '../Store';
+import {ITodoState} from '../states/TodoState';
 import {TodoComponent} from '../components/TodoComponent';
 
 export interface ITodoActions {
     addTodo: (v: string) => Action<string>;
 }
 
-function mapDispatchToProps(dispatch: any) {
+function mapDispatchToProps(dispatch: Dispatch<Action<any>>): ITodoActions {
     return {
         addTodo: (v: string) => dispatch(todoActions.addTodo(v)),
     };
 }
 
-function mapStateToProps(appState: IAppState) {
+function mapStateToProps(appState: IAppState): ITodoState {
     return Object.assign({}, appState.todo);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodoComponent);
+export default connect<ITodoState, ITodoActions>(mapStateToProps, mapDispatchToProps)(TodoComponent);
